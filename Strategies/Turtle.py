bf4405b21abaffb2b5e182257649bf8e4d8b113e
Jared <jaredwads@gmail.com>
2018-04-14 20:34:47 -0600

    Added multiple instruments, aggregators and breakout indicator support

--- Strategies/Turtle.py
+++ Strategies/Turtle.py
@@ -2,6 +2,7 @@ import json
 import threading
 
 from Events.CandleEvent import CandleEvent
+from Events.IndicatorEvent import IndicatorEvent
 from Events.SignalEvent import SignalEvent
 from Indicators.Breakout import Breakout
 from Models.rabbitmq_publisher import Publisher
@@ -13,66 +14,72 @@ from Strategies.IStrategy import IStrategy
 class Turtle(IStrategy):
     def __init__(self, instruments, duration=20, constrained=True):
         self.instruments = instruments
-        self.breakouts = dict([(i, Breakout(length=duration)) for i in instruments])
         self.publisher = Publisher()
         self.subscriber = None
-        self.in_market = False
+        self.in_market = dict([(i, False) for i in instruments])
         self.last_trade_profitable = dict([(i, False) for i in instruments])
 
     def subscribe(self):
-        self.subscriber = Subscriber('#.TICK')
+        self.subscriber = Subscriber('')
         print('Listening for TICKs...')
 
-        def consume_signal(ch, method, properties, data):
+        def consume_tick(ch, method, properties, data):
             candle = CandleEvent.from_json(json.loads(data.decode('utf8')))
             self.calculate_signals(candle)
 
-        self.subscriber.consume(consume_signal)
+        def consume_breakout(ch, method, properties, data):
+            indicator = IndicatorEvent.from_json(json.loads(data.decode('utf8')))
+            self.on_receive_breakout(indicator)
+
+        self.subscriber.consume_multiple(['#.H1.TICK', '#.BREAKOUT.*'], [consume_tick, consume_breakout])
 
     def calculate_signals(self, candle_event):
-        if candle_event.duration == Duration.DAY.name:
-            self.process_signal(candle_event)
-            self.increment_breakout(candle_event.instrument, candle_event.candle)
-
-
-    def increment_breakout(self, instrument, candle):
-        self.breakouts[instrument].tick(candle.close)
-
-    def process_signal(self, candle_event):
-        if self.in_market:
-            signal = self.should_add_units()
-        else:
-            signal = self.should_enter_market(candle_event)
-
-        if signal is not None:
-            print('Entering Market: ' + signal.to_json())
-            self.publisher.publish(candle_event.instrument + '.SIGNAL', signal.to_json())
-
-    def should_enter_market(self, candle_event):
         instrument = candle_event.instrument
-        breakout = self.breakouts[instrument]
 
-        if not breakout.ready():
-            return None
+        if instrument not in self.instruments:
+            return
 
-        if candle_event.candle.close > breakout.current_max:
-            return SignalEvent(instrument, None, 'BUY')
-        elif candle_event.candle.close < breakout.current_min:
-            return SignalEvent(instrument, None, 'SELL')
+        if self.should_add_units(instrument):
+            direction = self.calculate_add_units_direction(instrument)
+            self.send_signal(instrument, direction)
+
+        if self.should_exit(instrument):
+            self.exit(instrument)
+
+    def on_receive_breakout(self, indicator):
+        instrument = indicator.instrument
+        direction = indicator.value
+
+        if instrument not in self.instruments:
+            return
+
+        if self.should_enter_market(instrument, direction):
+            self.send_signal(instrument, direction)
+
+    def send_signal(self, instrument, direction):
+        self.publisher.publish(instrument + '.SIGNAL', SignalEvent(instrument, None, direction).to_json())
+
+    def should_enter_market(self, instrument, direction):
+        return True
+
+    def should_add_units(self, instrument):
+        return False
+
+    def calculate_add_units_direction(self, instrument):
         return None
 
-    def should_add_units(self):
-        return None
+    def should_exit(self, instrument):
+        if not self.in_market[instrument]:
+            return False
+        return False
+
+    def exit(self, instrument):
+        pass
 
 
 if __name__ == "__main__":
     instruments = ['EUR_USD']
 
-    turtle1 = Turtle(instruments, duration=20, constrained=True)
-    turtle2 = Turtle(instruments, duration=55, constrained=False)
+    turtle = Turtle(instruments, duration=20, constrained=True)
 
-    fast = threading.Thread(target=turtle1.subscribe, args=[])
-    slow = threading.Thread(target=turtle1.subscribe, args=[])
-
-    fast.start()
-    slow.start()
+    turtle.subscribe()
