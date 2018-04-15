bd38c7ec2a797b45ea2790648736b7b0a849f032
Jared <jaredwads@gmail.com>
2018-04-14 20:33:15 -0600

    added multiple consumers

--- Models/rabbitmq_subscriber.py
+++ Models/rabbitmq_subscriber.py
@@ -9,17 +9,40 @@ class Subscriber:
                                                                        , heartbeat_interval=600))
         self.channel = connection.channel()
         self.channel.exchange_declare(exchange='JIM', exchange_type='topic')
-        result = self.channel.queue_declare(exclusive=True)
 
-        self.queue_name = result.method.queue
-
-        self.channel.queue_bind(exchange='JIM',
-                                queue=self.queue_name,
-                                routing_key=listening_route)
+        self.listening_route = listening_route
 
     def consume(self, callback):
+        result = self.channel.queue_declare(exclusive=True)
+
+        queue_name = result.method.queue
+
+        self.channel.queue_bind(exchange='JIM',
+                                queue=queue_name,
+                                routing_key=self.listening_route)
+
         self.channel.basic_consume(callback,
-                                   queue=self.queue_name,
+                                   queue=queue_name,
                                    no_ack=True)
+
+        self.channel.start_consuming()
+
+    def consume_multiple(self, routes, callbacks):
+        if len(routes) != len(callbacks):
+            raise ValueError('there must be the same number of routes ({0}) and Callbacks({1})'
+                             , len(routes), len(callbacks))
+
+        for i in range(len(routes)):
+            result = self.channel.queue_declare(exclusive=True)
+
+            queue_name = result.method.queue
+
+            self.channel.queue_bind(exchange='JIM',
+                                    queue=queue_name,
+                                    routing_key=routes[i])
+
+            self.channel.basic_consume(callbacks[i],
+                                       queue=queue_name,
+                                       no_ack=True)
 
         self.channel.start_consuming()
